86e9766fbd45d0d04fbf30d2845aafb1df149024
Maynot2 <paulmanot@gmail.com>
2021-04-14 15:33:44 +0000

    Create script to compress web_static locally

---
+++ 1-pack_web_static.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+    Compress web_static to .tgz file
+"""
+
+from fabric.api import *
+from datetime import datetime
+
+
+def do_pack():
+    """compress web_static"""
+
+    try:
+        local("mkdir -p versions")
+        time_stamp = datetime.now().strftime('%Y%m%d%H%M%S')
+        path = "versions/web_static_{}.tgz web_static".format(time_stamp)
+        local("tar -cvzf {}".format(path))
+        return path
+    except:
+        return None
