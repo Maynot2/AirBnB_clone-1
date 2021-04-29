e280645494c61d79301164aee6a01a46ad56a908
Maynot2 <paulmanot@gmail.com>
2021-04-29 08:51:20 +0000

    Create new 'catch all' route

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,26 @@
+"""
+    A simple flask exemple with 3 routes
+"""
+
+from web_flask import app
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Says Hello on the home page"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Route to HBNB page"""
+    return 'HBNB'
+
+
+@app.route('/c/<path>', strict_slashes=False)
+def c_is(path):
+    """Route to /c/catch_all"""
+    return 'C {}'.format(path.replace("_", " "))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
