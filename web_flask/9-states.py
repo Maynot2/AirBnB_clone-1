dea8a3cd1a2d0b3afe68a701148fef3d58dc6f5e
Maynot2 <paulmanot@gmail.com>
2021-04-30 08:56:49 +0000

    Add more toutes and views

---
+++ web_flask/9-states.py
@@ -0,0 +1,31 @@
+"""
+    A simple flask exemple
+"""
+
+from web_flask import app
+from flask import render_template
+from models import storage
+from models.state import State
+
+
+@app.route('/states', strict_slashes=False)
+def states():
+    """Displays all the states"""
+    states = storage.all(State).values()
+    return render_template('9-states.html', states=states)
+
+
+@app.route('/states/<string:id>', strict_slashes=False)
+def given_state(id):
+    """Display a given state with its cities"""
+    key = 'State.{}'.format(id)
+    state = storage.all(State).get(key)
+    return render_template('9-states.html', states=state)
+
+@app.teardown_appcontext
+def rm_sqlalchemy_sess(self):
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
