d316f521e2137de333fd76e94a4898bdbe5d49a5
Maynot2 <paulmanot@gmail.com>
2021-04-30 07:33:08 +0000

    Add route an view

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,24 @@
+"""
+    A simple flask exemple
+"""
+
+from web_flask import app
+from flask import render_template
+from models import storage
+from models.state import State
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_state():
+    """Displays all cities in each states"""
+    states = storage.all(State).values()
+    return render_template('8-cities_by_states.html', states=states)
+
+
+@app.teardown_appcontext
+def rm_sqlalchemy_sess(self):
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
