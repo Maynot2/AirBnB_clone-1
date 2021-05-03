524f7f985c8bdf52572d5fbd67cc11ded58b53e4
Maynot2 <paulmanot@gmail.com>
2021-05-03 11:53:16 +0000

    Fix excutable path and import

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,12 +1,15 @@
+#!/usr/bin/python3
 """
     A simple flask exemple
 """
 
-from web_flask import app
+from flask import Flask
 from flask import render_template
 from models import storage
 from models.state import State
 from models.amenity import Amenity
+
+app = Flask(__name__)
 
 
 @app.route('/hbnb_filters', strict_slashes=False)
